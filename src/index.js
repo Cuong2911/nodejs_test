b53d842c53c9cc805d31a0288f25e607407e0a21
Cuong2911 <hvcuong29112001@gmail.com>
2023-10-27 21:40:27 +0700

    1

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import morgan from 'morgan';
+import { engine } from 'express-handlebars';
+
+
+const app = express();
+const port = 5000;
+
+app.use(express.static('./src/public/'));
+
+// Http logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './src/resources/views');
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+app.listen(port, () => {
+});
